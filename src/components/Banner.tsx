9f60c4c9d70c95f26782c70f393970bdde1fa275
Tyrone Ranson <mantis@Tyrones-MacBook-Pro.local>
2023-03-31 15:52:25 +0900

    update

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,28 +1,14 @@
-import { baseUrl } from "@/contants/movie"
-import { Movie } from "@/types/content"
 import Image from "next/image"
-import { useEffect, useState } from "react"
 import { FaPlay } from "react-icons/fa"
-import { InformationCircleIcon } from "@heroicons/react/24/solid"
 
 interface Props {
-  netflixOriginals: Movie[]
+  /* */
 }
 
-function Banner({ netflixOriginals}: Props) {
-  const [movie, setMovie] = useState<Movie | null>(null)
-
-  useEffect(() => {
-    setMovie(netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)])
-  }, [netflixOriginals])
-  console.log(movie)
+function Banner({}: Props) {
   return (
     <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
       <div className="absolute top-0 left-0 -z-10 h-[95vh]" style={{width:'calc(100% - 1px)'}}>
-        {/* <Image alt="" src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
-        fill
-        style={{objectFit:'cover'}}
-        /> */}
         <Image alt="" src="https://firebasestorage.googleapis.com/v0/b/dinami-27097.appspot.com/o/pexels-ketut-subiyanto-4350089.jpg?alt=media&token=223d84c0-4b49-4cfa-aebd-e2c1b7a17c0a"
         fill
         style={{objectFit: 'cover'}}
@@ -40,9 +26,6 @@ function Banner({ netflixOriginals}: Props) {
       <button className="bannerButton bg-white text-black hover:opacity-75">
         <FaPlay className="h-4 w-4 text-black md:h-7 md:w-7"/>Play
       </button>
-      {/* <button className="bannerButton bg-[gray]/70 hover:opacity-75">
-        <InformationCircleIcon className="h-5 w-5 md:h-8 md:w-8"/> More Info
-      </button> */}
     </div>
     </div>
   )
